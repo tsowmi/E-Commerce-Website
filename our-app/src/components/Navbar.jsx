3a9641f7a8e4685b580573b47063316d743a81ea
Sowmya <tuttaguntasowmya@gmail.com>
2023-03-21 22:30:49 +0530

    changed navbar links

--- our-app/src/components/Navbar.jsx
+++ our-app/src/components/Navbar.jsx
@@ -2,6 +2,7 @@ import { NavItem } from 'react-bootstrap';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import { Link } from 'react-router-dom'
 
 
 function CollapsibleExample() {
@@ -11,17 +12,17 @@ function CollapsibleExample() {
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className='me-auto' >
-            <Nav.Link className="categories" href="men">Men</Nav.Link>
-            <Nav.Link className="categories" href="women">Women</Nav.Link>
-            <Nav.Link className="categories" href="kids">Kids</Nav.Link>
-            <Nav.Link className="categories" href="electronics">Electronics</Nav.Link>
+            <Link to="/men" className="categories" style={{textDecoration:"none", color:"grey"}} >Men</Link>
+            <Link className="categories" to="/women" style={{textDecoration:"none", color:"grey"}}>Women</Link>
+            <Link className="categories" to="/kids" style={{textDecoration:"none", color:"grey"}}>Kids</Link>
+            <Link className="categories" to="/electronics" style={{textDecoration:"none", color:"grey"}}>Electronics</Link>
           </Nav>
         </Navbar.Collapse>
         <Navbar.Brand href="/" className='myBrand'>VS Shoppe</Navbar.Brand>
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="ms-auto">
-          <NavItem ><Nav.Link  className="categories" href="login">Login</Nav.Link></NavItem>
-          <NavItem ><Nav.Link  className="categories" href="signup">Signup</Nav.Link></NavItem>
+          <NavItem ><Link  className="categories" to="/login" style={{marginRight:"20px", textDecoration:"none", color:"grey"}}> Login</Link></NavItem>
+          <NavItem ><Link  className="categories" to="/signup" style={{textDecoration:"none", color:"grey"}}>Signup</Link></NavItem>
           </Nav>
         </Navbar.Collapse>
       </Container>
